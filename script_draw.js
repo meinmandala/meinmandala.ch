9422033aab135c44daad3e3da893bace7b82e364
meinmandala <129592258+meinmandala@users.noreply.github.com>
2023-04-29 22:39:45 +0200

    Add files via upload

--- script_draw.js
+++ script_draw.js
@@ -17,19 +17,4 @@ ${code_input.value}
 let debounce_update = null;
 let debounce_do = false;
 
-code_input.addEventListener('keyup',function() {
-  if(debounce_update) {
-    debounce_do = true;
-    return;
-  }
-  update();
-  debounce_update = setTimeout(function() {
-    debounce_update = null;
-    if(debounce_do) {
-      update();
-    }
-    debounce_do = false;
-  },500);
-})
-
 update();
